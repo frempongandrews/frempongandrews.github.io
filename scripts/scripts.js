bfa5912aa893c10362170bdbaa6e51d257f07b79
Andrews Frempong <frempongandrews@yahoo.com>
2016-11-08 15:06:20 +0000

    jquery menu for small screens completed

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,15 @@
 
+//opening menu on small screens
 $(".hamburger").on("click", function () {
     $(".main-menu-small").css({
         transform: "translateX(0)",
+    });
+});
+
+//closing menu on small screens
+
+$(".close-menu").on("click", function () {
+    $(".main-menu-small").css({
+        transform: "translateX(-100vw)"
     });
 });
