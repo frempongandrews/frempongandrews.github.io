1d521e204f1ae7dc6d1779fd71a0d942320331e0
Andrews Frempong <frempongandrews@yahoo.com>
2016-11-19 01:15:03 +0000

    added [adding and removing class to small screen menu on click]

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,15 +1,13 @@
 
 //opening menu on small screens
 $(".hamburger").on("click", function () {
-    $(".main-menu-small").css({
-        transform: "translateX(0)",
-    });
+    $(".main-menu-small").removeClass("close").addClass("open");
 });
 
 //closing menu on small screens
 
 $(".close-menu").on("click", function () {
-    $(".main-menu-small").css({
-        transform: "translateX(-100vw)"
-    });
+    $(".main-menu-small").removeClass("open").addClass("close");
 });
+
+
